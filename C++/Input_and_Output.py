f148d6bbeec5d4ff4e2da4ef927781911dc1d823
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2022-12-06 11:08:44 +0530

    Create Input_and_Output.py

---
+++ C++/Input_and_Output.py
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin>>a>>b>>c;
+    int sum;
+
+    sum = a+b+c;
+    cout<<sum;
+
+    return 0;
+
+}
