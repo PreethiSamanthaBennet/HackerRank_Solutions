1a7d8e748d9d5cd08f2b73fdbb08bd62016a0559
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2022-12-06 11:11:58 +0530

    Create Basic_Data_types.cpp

---
+++ C++/Basic_Data_types.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    int i;
+    cin>>i;
+    cout<<i<<endl;
+
+    long ld;
+    cin>>ld;
+    cout<<ld<<endl;
+
+    char ch;
+    cin>>ch;
+    cout<<ch<<endl;
+
+    float f;
+    cin>>f;
+    printf("%.3f", f);
+    
+    cout<<endl;
+    
+    double d;
+    cin>>d;
+    printf("%.9lf", d);
+
+    return 0;
+}
