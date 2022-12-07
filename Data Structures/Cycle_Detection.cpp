bc560a2587ecda9af0910badcac7aff05f0a789c
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2022-12-07 18:03:10 +0530

    Create Cycle_Detection.cpp

---
+++ Data Structures/Cycle_Detection.cpp
@@ -0,0 +1,17 @@
+bool has_cycle(SinglyLinkedListNode* head) {
+    if(head == NULL){
+        return 0;
+    }
+    SinglyLinkedListNode* slow = head;
+    SinglyLinkedListNode* fast = head;
+    
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(slow == fast){
+            return 1;
+        }
+    }
+    return 0;
+}
