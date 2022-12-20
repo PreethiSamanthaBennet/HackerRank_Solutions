577deb7e301a4ff25043fcdef6f049f75b6f1763
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2022-12-20 16:52:47 +0530

    Create 1D_Arrays_in_C.c

---
+++ 1D_Arrays_in_C.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    int sum = 0;
+        
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+  
+    printf("%d", sum);
+    return 0;
+}
