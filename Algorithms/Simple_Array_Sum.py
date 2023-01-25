145364ad38a6ab767709fa18a218de8bb81bd242
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-01-25 19:30:19 +0530

    Update Simple_Array_Sum.py

--- Algorithms/Simple_Array_Sum.py
+++ Algorithms/Simple_Array_Sum.py
@@ -1,25 +1,6 @@
-import math
-import os
-import random
-import re
-import sys
-
 def simpleArraySum(ar):
     sum_array = 0
     
     for i in range(len(ar)):
         sum_array += ar[i]
     return sum_array
-    
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input().strip())
-    
-    ar = list(map(int, input().rstrip().split()))
-
-    result = simpleArraySum(ar)
-
-    fptr.write(str(result) + '\n')
-    
-    fptr.close()
