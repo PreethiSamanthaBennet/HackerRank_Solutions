0acff0dd8f60bde6b2ef25a66114396f3166ae45
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-05-26 12:56:06 +0530

    Create Subarray_Division.py

---
+++ Algorithms/Subarray_Division.py
@@ -0,0 +1,11 @@
+def birthday(s, d, m):
+    i = 0
+    j = m
+    count = 0
+    
+    while j <= len(s):
+        if sum(s[i:j]) == d:
+            count += 1
+        i += 1
+        j += 1
+    return count
