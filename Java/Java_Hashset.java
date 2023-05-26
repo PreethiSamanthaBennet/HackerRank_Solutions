b7d5b06791c205e5990a2825ec974f59b7d304a9
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-05-26 12:53:55 +0530

    Create Java_Hashset.java

---
+++ Java/Java_Hashset.java
@@ -0,0 +1,5 @@
+HashSet<String> hs = new HashSet<>();
+        for(int i = 0; i < t; i++){
+            hs.add(pair_left[i]+" "+pair_right[i]);
+            System.out.println(hs.size());
+        }
