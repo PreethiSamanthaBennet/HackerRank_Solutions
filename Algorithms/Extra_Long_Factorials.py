4e0701ac3e127445f84e0afa29838ac14f73319e
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-06-02 16:18:08 +0530

    Update Extra_Long_Factorials.py

--- Algorithms/Extra_Long_Factorials.py
+++ Algorithms/Extra_Long_Factorials.py
@@ -1,8 +1,6 @@
 def extraLongFactorials(n):
-    if n == 0: return 0
-    res = 1
+    fact = 1
     
-    while n > 0:
-        res *= n
-        n -= 1
-    return res
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
